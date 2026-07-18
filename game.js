52304c954564944a0061ae0941d88dd4a96bb673
Md-Zayan00 <mdzayan449@gmail.com>
2026-07-18 22:34:34 +0530

    add enemy ai
    - Added complete sacrifice button functionality
    - Added complete revive button functionality
    - Added computer's turn and enemy auto play

--- game.js
+++ game.js
@@ -7,6 +7,9 @@ let text = document.getElementById("log")
 let attacking = 0
 let using = 0
 
+//Create counter for round number
+let roundNo = 1
+
 //Declare an array to store the last four messages
 let textLog = ["","",
     "","",""]
@@ -52,6 +55,8 @@ function endTurn() {
     attacking = 0
     using = 0
     setCheckboxes()
+    //Start computer's turn
+    compTurn()
 }
 
 //Choosing which hand to attack
@@ -120,20 +125,25 @@ function playerAttack() {
     }
     //For all other cases
     else if(attacking === -1 && using === -1){
+        messageLog("___round start___")
         computer.lhs = evaluateAttack(true, player.lhs, computer.lhs)
+        endTurn()
     }
     else if(attacking === -1 && using === 1){
+        messageLog("___round start___")
         computer.lhs = evaluateAttack(true, player.rhs, computer.lhs)
+        endTurn()
     }
     else if(attacking === 1 && using === -1){
+        messageLog("___round start___")
         computer.rhs = evaluateAttack(true, player.lhs, computer.rhs)
+        endTurn()
     }
     else if(attacking === 1 && using === 1){
+        messageLog("___round start___")
         computer.rhs = evaluateAttack(true, player.rhs, computer.rhs)
+        endTurn()
     }
-
-    //End the player's turn
-    endTurn()
 }
 
 //Declare function to evaluate attack and return it
@@ -161,7 +171,11 @@ function evaluateAttack(isPlayer, x, y){
     if(total === 5){
         total = 0
     }
-    //If not, is it a 10 or above?
+    //If not, is it a 14
+    else if(total === 14){
+        total = 0
+    }
+    //otherwise negate 9 from it
     else if(total >= 10){
         total -= 9
     }
@@ -181,5 +195,160 @@ function sacrificeHand(){
     //Check if the combining hand is 5
     else if(player.lhs + player.rhs === 5){
         messageLog("Resulting hand will die!")
+    }
+    //Normal cases
+    else {
+        //Add both hands into one and set the other to 0
+        messageLog("___round start___")
+        player.lhs = evaluateAttack(true, player.lhs, player.rhs)
+        player.rhs = 0
+        endTurn()
+    }
+}
+
+//Declare function for reviving dead hand
+function reviveHand(){
+    //Start creating the message
+    let temp = "Player: "
+    
+    //Check if both hands are alive
+    if(player.rhs > 0 && player.lhs > 0){
+        messageLog("All hands are alive!")
+    }
+    //Check if a hand dies upon reviving the other
+    else if(player.rhs === 6 || player.lhs === 6){
+        messageLog("Proceeding will kill a hand!")
+    }
+    //check if the only alive hand is 1
+    else if(player.lhs === 1 || player.rhs === 1){
+        messageLog("Cannot Revive Hand!")
+    }
+    //otherwise proceed
+    else if(player.rhs === 0){
+        temp += emoji[player.lhs] + " = "
+        player.lhs -= 1
+        player.rhs += 1
+        temp += emoji[player.lhs] + " + " + emoji[player.rhs]
+        messageLog("___round start___")
+        messageLog(temp)
+        endTurn()
+    }
+    else if(player.lhs === 0){
+        temp += emoji[player.rhs] + " = "
+        player.rhs -= 1
+        player.lhs += 1
+        temp += emoji[player.rhs] + " + " + emoji[player.lhs]
+        messageLog("___round start___")
+        messageLog(temp)
+        endTurn()
+    }
+    
+}
+
+//Declare function to evaluate computer's moves
+function compTurn(){
+    //Priority 1: Kill a hand
+    //Check if the sum of any combination will give a 5
+    if(computer.lhs + player.lhs === 5){
+        player.lhs = evaluateAttack(false, computer.lhs, player.lhs)
+    }
+    else if(computer.rhs + player.lhs === 5){
+        player.lhs = evaluateAttack(false, computer.rhs, player.lhs)
+    }
+    else if(computer.lhs + player.rhs === 5){
+        player.rhs = evaluateAttack(false, computer.lhs, player.rhs)
+    }
+    else if(computer.rhs + player.rhs === 5){
+        player.rhs = evaluateAttack(false, computer.rhs, player.rhs)
+    }
+    //Priority 2: Weaken a hand
+    //Check if the sum of any combination yields a 10 or above
+    else if(computer.lhs + player.lhs >= 10){
+        player.lhs = evaluateAttack(false, computer.lhs, player.lhs)
+    }
+    else if(computer.rhs + player.lhs >= 10){
+        player.lhs = evaluateAttack(false, computer.rhs, player.lhs)
+    }
+    else if(computer.lhs + player.rhs >= 10){
+        player.rhs = evaluateAttack(false, computer.lhs, player.rhs)
+    }
+    else if(computer.rhs + player.rhs >= 10){
+        player.rhs = evaluateAttack(false, computer.rhs, player.rhs)
+    }
+    //Priority 3: Revive a hand if possible
+    //Check if a hand is dead, no hand yields a 6, and no combination can kill it
+    else if(computer.lhs === 0 && computer.rhs !== 6 && 
+        player.lhs + computer.rhs - 1 !== 5 && 
+        player.rhs + computer.rhs - 1 !== 5 && 
+        player.lhs + 1 !== 5 && computer.rhs > 1 && 
+        player.rhs + 1 !== 5){
+            let temp = "Computer: " + emoji[computer.rhs] + " = "
+            computer.rhs -= 1
+            computer.lhs += 1
+            temp = emoji[computer.rhs] + " + " + emoji[computer.lhs]
+            messageLog(temp)
+        }
+    else if(computer.rhs === 0 && computer.lhs !== 6 && 
+        player.lhs + computer.lhs - 1 !== 5 && 
+        player.rhs + computer.lhs - 1 !== 5 && 
+        player.lhs + 1 !== 5 && computer.lhs > 1 && 
+        player.rhs + 1 !== 5){
+            let temp = "Computer: " + emoji[computer.lhs] + " = "
+            computer.lhs -= 1
+            computer.rhs += 1
+            temp = emoji[computer.lhs] + " + " + emoji[computer.rhs]
+            messageLog(temp)
+        }
+    //Priority 4: Attack any hand at random
+    else{
+        randomChoice()
+    }
+    endRound()
+}
+
+//Declare function to end the round
+function endRound(){
+    roundNo++
+    player.Left.innerText = emoji[player.lhs]
+    player.Right.innerText = emoji[player.rhs]
+    computer.Left.innerText = emoji[computer.lhs]
+    computer.Right.innerText = emoji[computer.rhs]
+    messageLog("___round end___")
+}
+
+//Declare a function to randomly choose for the computer
+function randomChoice(){
+    //Declare two hands
+    let handA = Math.floor(Math.random()*2) + 1
+    let handB = Math.floor(Math.random()*2) + 1
+
+    //Check if the player only has one hand
+    if(player.lhs === 0){
+        handB = 2
+    }
+    else if(player.rhs === 0){
+        handB = 1
+    }
+
+    //Check if the computer only has one hand
+    if(computer.lhs === 0){
+        handA = 2
+    }
+    else if(computer.rhs === 0){
+        handA = 1
+    }
+    
+    //Attack the randomly chosen hands
+    if(handA === 1 && handB === 1){
+        player.lhs = evaluateAttack(false, computer.lhs, player.lhs)
+    }
+    else if(handA === 1 && handB === 2){
+        player.rhs = evaluateAttack(false, computer.lhs, player.rhs)
+    }
+    else if(handA === 2 && handB === 1){
+        player.lhs = evaluateAttack(false, computer.rhs, player.lhs)
+    }
+    else if(handA === 2 && handB === 2){
+        player.rhs = evaluateAttack(false, computer.rhs, player.rhs)
     }
 }
